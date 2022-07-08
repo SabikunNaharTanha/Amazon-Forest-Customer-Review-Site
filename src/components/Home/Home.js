c49574111ced2702c07105cf060bfd522bc1afce
SabikunNaharTanha <sabikunnahartanha6@gmail.com>
2022-07-08 10:51:48 +0600

    created navbar and set the routes

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className="details-container">
+
+            </div>
+            <div className="image-container">
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
