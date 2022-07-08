b500c5f7fe07df0d9baab4107ed7f922167ca967
SabikunNaharTanha <sabikunnahartanha6@gmail.com>
2022-07-08 20:17:52 +0600

    blog added

---
+++ src/Hook/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h2>Contact:</h2>
+            <p>Email: <a href='/'>amazonforest@gmail.com</a></p>
+
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>Q @ A</h2>
+        <div className='blogs'>
+            <div className='blog-1'>
+                <h2 className='text-2xl text-rose-800 font-semibold mb-3'>What is Context API. Write its details</h2>
+                <p className='text-lg'>Context API allows us to pass the data through the component where props is not passes to every child or level. In short, without defining props in every level of a tree, context API allows us to show the data exactly to the level or child section we want to show it. <br /> To dot This we need to use createContext(), use .Provider from the top level. And where in the level it is needed, just need to use it.</p>
+            </div>
+            <div className='blog-2'>
+                <h2 className='text-2xl text-rose-800 font-semibold mb-3'>What is Semantic Tag</h2>
+                <p className='text-lg'>Semantic tags are the tags that are meaningful. We can identify with tag it is and what is its purpose. For example, tags like, Header ,footer,body,title etc.
+                    <br /><br />
+                    On the contrary, tags like a,p,div doesn't give us such meanings.</p>
+            </div>
         </div>
     );
 };
