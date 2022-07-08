d4e3aadf43e3fafc78dc58a4fba1fabe2481ce20
SabikunNaharTanha <sabikunnahartanha6@gmail.com>
2022-07-08 13:29:31 +0600

    Home Page Created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Review from '../Review/Review';
+import './Home.css';
 
 const Home = () => {
+    const [reviews, setReview] = useState([]);
+
+    useEffect(() => {
+        fetch('generated.json')
+            .then(res => res.json())
+            .then(data => setReview(data))
+    }, []);
+
+
     return (
-        <div className='home'>
-            <div className="details-container">
+        <div>
+            <div className='home'>
+                <div className="details-container">
+                    <h1 className='text-3xl font-bold text-rose-800 mb-5'>Amazon Forest Customer Review Site</h1>
+                    <p className='text-lg text-gray-800'>Amazon Forest Customer Review is a site to show the customer satisfaction level for our company through their reviews. Explore the reviews and connect with us to travel the world!</p>
+                    <Link to='/about'>
+                        <button className='p-3 bg-slate-200 rounded-lg m-3 text-rose-900 text-lg font-semibold'>Learn More</button>
+                    </Link>
+                </div>
+                <div className="image-container">
+                    <img src={'Home.jpg'} alt="" />
 
+                </div>
             </div>
-            <div className="image-container">
+            <h1 className='text-2xl font-bold text-rose-800'>Customer Review</h1>
+            <div className='review-container'>
+                {
+                    reviews.map(review => <Review
+                        key={review.id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
 
-            </div>
         </div>
     );
 };
---
+++ src/components/Review/Review.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+import './Review.css';
+
+const Review = ({ review }) => {
+    const { picture, name, ratings, age, text } = review;
+    return (
+        <div className='reviews'>
+            <img src={picture} alt="" />
+            <h3 className='font-semibold text-lg text-pink-800'>{name}</h3>
+            <p><small>Age: {age}</small></p>
+            <p><small>Ratings: {ratings}</small></p>
+            <p><small>
+                {text.length > 50 ? text.slice(0, 50) + '...' : text}
+            </small></p>
+            <CustomLink to='/review'>
+                <button className='p-2 bg-slate-200 rounded-lg m-3 text-rose-900 text-lg font-semibold'>Read More</button>
+            </CustomLink>
+
+        </div>
+    );
+};
+
+export default Review;
