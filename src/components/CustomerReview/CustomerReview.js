5b4d040c202015af9387d7a79cc9359ed4fb3efe
SabikunNaharTanha <sabikunnahartanha6@gmail.com>
2022-07-09 12:37:00 +0600

    reviews set

--- src/components/CustomerReview/CustomerReview.js
+++ src/components/CustomerReview/CustomerReview.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import Reviews from '../../Hook/Footer/Reviews/Reviews';
-import Review from '../Review/Review';
+import ReviewCard from '../ReviewCard/ReviewCard';
 import './CustomerReview.css'
 
 const CustomerReview = () => {
     const [reviews, setReviews] = Reviews();
+
     return (
         <div className='Customer-review'>
             <h1 className='m-20 text-3xl font-bold text-rose-800'>Customer Reviews</h1>
-
             <div className='review-box'>
                 {
-                    reviews.map(review => <Review key={review.id}
-                        review={review}
-                    ></Review>)
+                    reviews.map(review => <ReviewCard key={review.id} review={review}></ReviewCard>)
                 }
-
             </div>
         </div>
     );
---
+++ src/components/ReviewCard/ReviewCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ReviewCard.css';
+
+const ReviewCard = ({ review }) => {
+    const { name, age, ratings, picture, text } = review;
+    return (
+        <div className='review-card'>
+            <img src={picture} alt="" />
+            <div className='review-text'>
+                <h2 className='text-xl text-teal-800 font-semibold'>{name}</h2>
+                <p><small>Age: {age}</small></p>
+                <p><small>Ratings: {ratings}</small></p>
+                <p><small>{text}</small></p>
+            </div>
+
+        </div>
+    );
+};
+
+export default ReviewCard;
